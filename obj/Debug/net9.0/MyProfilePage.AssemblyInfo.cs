2f55e826d283c9d45b3c888031deb0c605c21912
sayukky0211 <kagura.moene1185@gmail.com>
2025-08-25 17:38:17 +0900

    “更新”

--- obj/Debug/net9.0/MyProfilePage.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyProfilePage.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProfilePage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48a960899d3ed5a41d2fa00be231c98b31bd29c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc902b737eca71170b6a9cb403c4b19aad1de9f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProfilePage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProfilePage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
