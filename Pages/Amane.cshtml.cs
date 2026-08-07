09cfbbd236d49d043403e2072b33b08c3f3a6471
sayukky0211 <kagura.moene1185@gmail.com>
2026-08-08 05:19:27 +0900

    “更新”

---
+++ Pages/Amane.cshtml.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class amaneModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
--- obj/Debug/net9.0/MyProfilePage.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyProfilePage.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProfilePage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97fa69b131da5afc8055794473251284a98db969")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dd04b215e6ce0593e2a293512cb7bf9e9aca9cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProfilePage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProfilePage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
